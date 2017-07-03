a275a6486f89ef6d5679b179da64c60e1f35c45c
Dimitar Nestorov <opensource@dimitarnestorov.com>
2017-07-03 20:17:05 +0300

    cropEXIF was a service

--- source/js/init.js
+++ source/js/init.js
@@ -4,7 +4,7 @@ angular.module(`uiCropper`, [])
     .factory(`cropAreaCircle`, require(`./classes/crop-area-circle`))
     .factory(`cropAreaSquare`, require(`./classes/crop-area-square`))
     .factory(`cropAreaRectangle`, require(`./classes/crop-area-rectangle`))
-    .factory(`cropEXIF`, require(`./classes/crop-exif`))
+    .service(`cropEXIF`, require(`./classes/crop-exif`))
     .factory(`cropArea`, require(`./classes/crop-area`))
     .factory(`cropCanvas`, require(`./classes/crop-canvas`))
     .directive(`uiCropper`, require(`./ui-cropper`));
